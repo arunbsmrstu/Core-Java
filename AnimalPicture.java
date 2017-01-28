e7d5e1e23eea4b314381c977d10498e823adc501
Arun Biswas <arunbsmrstu@gmail.com>
2017-01-29 01:33:06 +0600

    Animal picture
    Using System.out.print.ln

---
+++ AnimalPicture.java
@@ -0,0 +1,14 @@
+class AnimalPicture{
+	
+	public static void main(String as[]){
+		System.out.println("             ");
+		System.out.println("\t    ,  ,");
+		System.out.println("\t     --");
+		System.out.println("\t    (oo)");
+		System.out.println("    +========\\/");
+		System.out.println("   / || %%% ||");
+		System.out.println("  *  ||-----||");
+		System.out.println("     \"\"     \"\"");
+	}
+
+}
