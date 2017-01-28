731b961342642c78a2faa763d9b7a4ddb3663f99
Arun Biswas <arunbsmrstu@gmail.com>
2017-01-29 01:06:34 +0600

    Veriable
    Veriable

--- Veriable.java
+++ Veriable.java
@@ -1,7 +1,9 @@
-class PrimitiveTypesMinMax{
+class Veriable{
 	
 	public static void main(String as[]){
-		System.out.println("int(min) = "+Integer.MIN_VALUE);
+		number=10;
+		number=number+1;
+		System.out.println("Sumation of number : " number);
 	}
 
 }
